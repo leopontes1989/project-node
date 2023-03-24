8d5b541c973b3082e53bd01ddcd28bee35af895f
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-23 23:31:30 -0300

    Route query and header params. Initialize project fixed

--- src/server.js
+++ src/server.js
@@ -8,8 +8,14 @@ app.get('/', (request, response) => {
     response.json({name: 'Leonardo', age: 33});
 });
 
-app.post('/userdata', (request, response) => {
+app.get('/users', (request, response) => {
+    response.json([{name: 'Leonardo', age: 33}, {name: 'Lina', age: 29}]);
+});
+
+app.post('/userdata/:id/:email', (request, response) => {
     console.log(request.body);
+    console.log(request.params);
+    console.log(request.headers);
     response.status(200).json({sucess: true})
 })
 
