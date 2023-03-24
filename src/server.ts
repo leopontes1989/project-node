1f650b852c742f80ad537e3d92520ecf2c147a78
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-24 00:17:29 -0300

    TS dev

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-const { request, response } = require('express');
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (request, response) => {
     response.json({name: 'Leonardo', age: 33});
+});
+
+app.get('/users/new', (request, response) => {
+    response.json([{name: 'Leonardo', age: 33}, {name: 'Lina', age: 29}]);
 });
 
 app.get('/users', (request, response) => {
