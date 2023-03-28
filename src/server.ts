c47d3ca7f27479ccf9f6e56765b8588cc0869e6d
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-28 08:11:06 -0300

    Password crypt

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,34 @@
 import express from 'express';
+import { pool } from './mysql'
+import { v4 as uuidv4 } from 'uuid';
+import { hash } from 'bcrypt';
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    response.json({name: 'Leonardo', age: 33});
-});
-
-app.get('/users/new', (request, response) => {
-    response.json([{name: 'Leonardo', age: 33}, {name: 'Lina', age: 29}]);
-});
-
-app.get('/users', (request, response) => {
-    response.json([{name: 'Leonardo', age: 33}, {name: 'Lina', age: 29}]);
-});
-
-app.post('/userdata/:id/:email', (request, response) => {
-    console.log(request.body);
-    console.log(request.params);
-    console.log(request.headers);
-    response.status(200).json({sucess: true})
+app.post('/user', (request, response) => {
+    const { name, email, password }= request.body;
+    pool.getConnection((err: any, connection: any) => {
+        hash(password, 10, (err, hash) => {
+            if(err) {
+                return response.status(500).json({err});
+            }
+                connection.query(
+                'INSERT INTO users (user_id, name, email, password) VALUES (?, ?, ?, ?)',
+                [uuidv4(), name, email, hash],
+                (error: any, result: any, fields: any) => {
+                    connection.release();
+                    if (error) {
+                        return response.status(400).json({error});
+                    }
+                    response.status(200).json({success: true});
+                }
+            )
+        })
+        
+    })
 })
+
+
 
 app.listen(4000);
